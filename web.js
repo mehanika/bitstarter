9a87e3cfc00ecceeeba35bc7663dfe13e92c52df
Josue <mehanika91@gmail.com>
2013-07-15 00:17:10 +0000

    Modificando puerto de acceso

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var buffer = fs.readFileSync("index.html");
 response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
